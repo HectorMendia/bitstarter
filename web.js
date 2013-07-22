ba5a3ea1deca0ef3950aa900f95dc8c17f7374b0
Hector Mendia <hector.mendia@gmail.com>
2013-07-22 19:11:03 +0000

    Update web

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer(256);
+  response.send(buffer.toString('utf-8',fs.readFileSync('index.html')));  
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
